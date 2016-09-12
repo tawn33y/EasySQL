09d927538b8ad268119887ac26ac67c53427284e
tawn33y <tawneyk2@gmail.com>
2016-09-12 04:48:54 -0700

    Add files via upload

---
+++ core/db_connect.php
@@ -0,0 +1,45 @@
+<?php
+/*
+---------------------------------------------------------------------------------------------------------------------
+|-------------------------------------------------------------------------------------------------------------------|
+|      Swift_db v0.01                                                                                               |
+|-------------------------------------------------------------------------------------------------------------------|
+|            * Created by K Tony (Twitter: @tawn33y) . 2016                                                         |
+|            * Distributed under the Open Source License                                                            |
+|            * For instructions on how to use this library, open the 'readme.txt' file in the root directory        |
+|-------------------------------------------------------------------------------------------------------------------|
+---------------------------------------------------------------------------------------------------------------------
+*/
+
+
+
+/*-------------------------------------------------------------------------------------------------------------
+	ABOUT
+	- This file sets up the database connection
+
+	Note:
+	- In production, it is recommended to put all credentials in a single separate file
+	- Also remember to exclude this file from source code managers like Git and SVN.
+-------------------------------------------------------------------------------------------------------------*/
+
+$db_server 		= "localhost";	// database server
+$db_user 			= "root"; 			// database user
+$db_password 	= ""; 					// database password
+$db_name		 	= "sample_db"; 	// database name
+
+try {
+	// connect to database
+	$conn = new PDO("mysql:host=". $db_server . ";dbname=" . $db_name, $db_user, $db_password);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// echo "Connected successfully";
+}
+catch(PDOException $e) {
+	// return error message if connection failed
+	echo "Sorry we're experiencing connection problems: " . $e->getMessage();
+}
+
+// uncomment the line below to close the connection
+// $conn = null;
+
+?>
